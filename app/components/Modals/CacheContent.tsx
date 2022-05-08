c5e8c54b0399b2d7d89ae3194bde6945dcb21c58
aklevecz <teh@raptor.pizza>
2022-05-08 12:43:57 -0700

    logout bug

--- app/components/Modals/CacheContent.tsx
+++ app/components/Modals/CacheContent.tsx
@@ -97,49 +97,55 @@ export default function CacheContentModal({
 
   const claim = () => {
     setFetchingLocation(true);
-    if (navigator && navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        try {
-          const timestamp = position.timestamp;
-          const coords = position.coords;
-          const o = {
-            accuracy: coords.accuracy,
-            altitude: coords.altitude,
-            altitudeAccuracy: coords.altitudeAccuracy,
-            heading: coords.heading,
-            latitude: coords.latitude,
-            longitude: coords.longitude,
-            speed: coords.speed,
-          };
-          const userLocation = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          console.log(haversineDistance(userLocation, data.cache.location));
 
-          const res = await claimCache(
-            data.cache.id,
-            data.cache.location,
-            userLocation,
-            {
-              timestamp,
-              o,
+    if (navigator && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          try {
+            const timestamp = position.timestamp;
+            const coords = position.coords;
+            const o = {
+              accuracy: coords.accuracy,
+              altitude: coords.altitude,
+              altitudeAccuracy: coords.altitudeAccuracy,
+              heading: coords.heading,
+              latitude: coords.latitude,
+              longitude: coords.longitude,
+              speed: coords.speed,
+            };
+            const userLocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+            console.log(haversineDistance(userLocation, data.cache.location));
+
+            const res = await claimCache(
+              data.cache.id,
+              data.cache.location,
+              userLocation,
+              {
+                timestamp,
+                o,
+              }
+            );
+            setFetchingLocation(false);
+            setTxState(TxState.Mining);
+            if (res.tx) {
+              setTxHash(res.tx.hash);
+            } else {
+              setTxState(TxState.Error);
+              setError({ message: res.message, error: res.error });
             }
-          );
-          setFetchingLocation(false);
-          setTxState(TxState.Mining);
-          if (res.tx) {
-            setTxHash(res.tx.hash);
-          } else {
+          } catch (e) {
             setTxState(TxState.Error);
-            setError({ message: res.message, error: res.error });
+            alert(e);
+            // setError({ message: e, error: res.error });
           }
-        } catch (e) {
-          setTxState(TxState.Error);
+        },
+        (e) => {
           alert(e);
-          // setError({ message: e, error: res.error });
         }
-      });
+      );
     } else {
       alert(
         "This browser does not support location services, or you have turned them off"
