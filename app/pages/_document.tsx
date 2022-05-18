2c8d161b15df219d00049c1b09a38cebc4e04cd7
aklevecz <teh@raptor.pizza>
2022-05-17 20:19:20 -0700

    gtag

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -10,6 +10,23 @@ class Document extends NextDocument<Props> {
           <link rel="icon" type="image/x-icon" href="/egg.png" />
           <link href="https://use.typekit.net/lfx3jsx.css" rel="stylesheet" />
           <script src="https://air.yaytso.art/twitter.js" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-48WHPVPCZT"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+            
+            gtag("config", "G-48WHPVPCZT");
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
