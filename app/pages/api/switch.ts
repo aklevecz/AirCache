568ba8442d95d3e6cf818075690bc32a2af82265
aklevecz <teh@raptor.pizza>
2022-06-30 09:42:26 -0700

    ogimage

---
+++ app/pages/api/switch.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+import db from "../../libs/db";
+import { cacheByGroupTableName, ZERO_ADDRESS } from "../../libs/constants";
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+type Data = {
+  dbRes: any;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // if (!req.headers.authorization) return res.status(405).end();
+  try {
+    //   const user = await jwt.verify(
+    //     req.headers.authorization as string,
+    //     JWT_SECRET
+    //   );
+    // const { cacheId, tokenId, tokenAddress, groupName } = req.body;
+    // const cacheId = 23;
+    // const tokenId = 0;
+    // const tokenAddress = ZERO_ADDRESS;
+    const TableName = cacheByGroupTableName;
+    for (let i = 118; i <= 150; i++) {
+      const params = {
+        TableName,
+        Key: {
+          cacheId: i.toString(),
+          groupName: "blackbeard",
+        },
+        UpdateExpression: "set groupName = :groupName",
+        ExpressionAttributeValues: {
+          ":groupName": "myosin-yacht",
+        },
+      };
+      const dbRes = await db.update(params).promise();
+    }
+
+    res.status(200).end();
+  } catch (e) {
+    console.log(e);
+    res.status(405).end();
+  }
+}
