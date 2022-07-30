43c52b04ec24909f5ebc102d926a242d57f1c02d
aklevecz <teh@raptor.pizza>
2022-07-29 18:04:20 -0700

    mmm

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites() {
+  redirects() {
     return {
       beforeFiles: [
         // if the host is `app.acme.com`,
