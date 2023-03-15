daf9c5b54f1436d1151d3ea8c902aa8a1b1df6a2
Ariel Klevecz <teh@raptor.pizza>
2023-03-14 22:59:16 -0700

    loc

--- app/pages/api/loc.ts
+++ app/pages/api/loc.ts
@@ -5,5 +5,5 @@ type Data = any;
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data | any>) {
   const loc = res.getHeader("loc");
   const device = res.getHeader("device");
-  return res.json({ loc, device });
+  return res.send(JSON.stringify(loc) + JSON.stringify(device));
 }
