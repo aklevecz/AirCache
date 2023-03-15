8e3445c240017d676e8c7e9e8774b14ddd5184d7
Ariel Klevecz <teh@raptor.pizza>
2023-03-14 23:35:26 -0700

    loc

--- app/pages/api/loc.ts
+++ app/pages/api/loc.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 type Data = any;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data | any>) {
+  console.log(res);
   const loc = res.getHeader("loc");
   const device = res.getHeader("device");
   console.log(loc, device);
