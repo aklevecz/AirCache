b560526901a54144b5d7da19d92ac07f2dbae8a4
Ariel Klevecz <teh@raptor.pizza>
2023-03-17 01:56:35 -0700

    hoodies

--- app/pages/api/login.ts
+++ app/pages/api/login.ts
@@ -11,29 +11,26 @@ type Data = {
   token: string;
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   // DYNAMIC MAGIC IF THE POST BODY HAS REFERENCES THE MAGIC APP
-  if (req.method !== "POST" || !req.headers.authorization)
-    return res.status(405).end();
+  if (req.method !== "POST" || !req.headers.authorization) return res.status(405).end();
   const hunt = req.body.destination.replace(/\//g, "");
   let magicSecret = MAGIC_SECRET;
   const keys = appKeys[hunt];
+  console.log(keys);
   if (keys) {
     magicSecret = keys.secret;
   }
   // monkey patching for connect
-  let user:any = {publicAddress:""}
-  if (ethers.utils.isAddress(req.headers.authorization.split('Bearer ')[1])){
-    user.publicAddress = req.headers.authorization.split('Bearer ')[1]
+  let user: any = { publicAddress: "" };
+  if (ethers.utils.isAddress(req.headers.authorization.split("Bearer ")[1])) {
+    user.publicAddress = req.headers.authorization.split("Bearer ")[1];
   } else {
-  let magic = new Magic(magicSecret);
-  const did = magic.utils.parseAuthorizationHeader(req.headers.authorization);
-  user = await magic.users.getMetadataByToken(did);
+    let magic = new Magic(magicSecret);
+    const did = magic.utils.parseAuthorizationHeader(req.headers.authorization);
+    user = await magic.users.getMetadataByToken(did);
   }
-  console.log('iser', user)
+  console.log("iser", user);
   var token = jwt.sign(user, JWT_SECRET, { expiresIn: "7d" });
   res.status(200).json({ token });
 }
