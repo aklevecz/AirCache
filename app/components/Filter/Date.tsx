ebd608a4bde02bf72cf32e8955f77a1952802c72
aklevecz <teh@raptor.pizza>
2023-04-07 23:04:58 -0700

    move filter down

--- app/components/Filter/Date.tsx
+++ app/components/Filter/Date.tsx
@@ -28,12 +28,12 @@ export default function FilterDate({ filter, applyFilter, dates }: Props) {
 
   return (
     <>
-      <div className="absolute flex flex-col gap-2 right-2 top-20 z-10 w-[95px]">
+      <div className="absolute flex flex-col gap-2 right-2 top-32 z-10 w-[95px]">
         <div onClick={toggleOpen} className="bg-white text-black p-2 cursor-pointer ">
           {!open ? filter || "All" : "Pick one!"}
         </div>
       </div>
-      <motion.div className="absolute z-10 right-2 top-[140px]" initial={{ opacity: 0 }} exit={{ opacity: 0 }} animate={controls}>
+      <motion.div className="absolute z-10 right-2 top-[180px]" initial={{ opacity: 0 }} exit={{ opacity: 0 }} animate={controls}>
         <div onClick={() => onApply("")} className={clsx(filter === "" ? "bg-red-500" : "bg-white", "text-black p-2 cursor-pointer")}>
           All
         </div>
